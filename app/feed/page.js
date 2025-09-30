c513f277e6a46fc586f7bc1e53b3c508079e33c4
Francesco De Vivo <francescodvf@yahoo.it>
2025-09-30 17:59:50 +0200

    Practice on image optimization and metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,21 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata = {
+//   title: 'All Posts',
+//   description: 'Browse all posts by all users',
+// };
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+
+  return {
+    title: `Browse All Our ${numberOfPosts} Posts.`,
+    description: 'Browse all posts by all users',
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
